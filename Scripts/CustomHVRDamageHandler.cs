52f67738c7ec0de4056a8b2b5610d8e7702b006c
CurtisNaples <curtis_fanning@hotmail.com>
2023-12-16 14:47:28 -0800

    added features to damage handler for revive and health caps

--- Scripts/CustomHVRDamageHandler.cs
+++ Scripts/CustomHVRDamageHandler.cs
@@ -4,8 +4,10 @@ using UnityEngine.Events;
 
 public class CustomHVRDamageHandler : HVRDamageHandler
 {
+    public float MaxLife = 100f;
     public UnityEvent<float, Vector3, Vector3> ServerDamageTaken = new UnityEvent<float, Vector3, Vector3>();
     public UnityEvent<float, Vector3, Vector3> ClientDamageTaken = new UnityEvent<float, Vector3, Vector3>();
+    
     protected NetworkDamageHandler networkDamageHandler;
 
     private void Awake()
@@ -20,14 +22,43 @@ public class CustomHVRDamageHandler : HVRDamageHandler
         {
             //Debug.Log("Server Damage");
             base.TakeDamage(damage);
+            CheckForRevive();
+            EnforceLifeLimit();
         }
     }
+
     //Clients should call this to update the health from damage
     public virtual void TakeNetworkDamage(float damage, Vector3 hitPoint, Vector3 direction)
     {
         //Debug.Log("Network Damage");
         base.TakeDamage(damage);
         ClientDamageTaken.Invoke(damage, hitPoint, direction);
+        CheckForRevive();
+        EnforceLifeLimit();
+    }
+
+
+    private void CheckForRevive()
+    {
+        if (Desctructible.Destroyed && Life > 0)
+        {
+            var playerdDeathDestructible = Desctructible as PlayerDeathDestructible;
+            if (playerdDeathDestructible)
+            {
+                playerdDeathDestructible.Revive();
+            }
+        }
+    }
+    private void EnforceLifeLimit()
+    {
+        if (Life < 0)
+        {
+            Life = 0;
+        }
+        else if (Life > MaxLife)
+        {
+            Life = MaxLife;
+        }
     }
 
     public override void HandleDamageProvider(HVRDamageProvider damageProvider, Vector3 hitPoint, Vector3 direction)
