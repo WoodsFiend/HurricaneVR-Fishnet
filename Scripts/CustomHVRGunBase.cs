7000c5001b7424e046a39d6122180ef0fcebf434
CurtisNaples <curtis_fanning@hotmail.com>
2023-12-29 20:34:20 -0800

    add support for pump and bolt action weapons

--- Scripts/CustomHVRGunBase.cs
+++ Scripts/CustomHVRGunBase.cs
@@ -1,5 +1,8 @@
 
 
+using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using HurricaneVR.Framework.Weapons;
 using HurricaneVR.Framework.Weapons.Guns;
 using UnityEngine;
 
@@ -9,16 +12,66 @@ public class CustomHVRGunBase : HVRGunBase
     private bool ejectCasingOnHandleEjected = false;
     [SerializeField]
     private bool enableChamberedOnHandleEjected = false;
+    [SerializeField]
+    private bool hasChamberedAmmo = false;
+
+    //This should be controlled by the ammo being shot
+    [SerializeField]
+    private int NumberOfPellets = 1;
+    [SerializeField]
+    private float ShotRadius = 0.05f;
 
     protected override void Awake()
     {
         base.Awake();
+        if (hasChamberedAmmo)
+        {
+            Ammo = GetComponent<HVRShotgunMagazine>();
+        }
     }
     //Custom entry point for networked shooting
     public void NetworkShoot()
     {
         Shoot();
     }
+
+    protected override void OnFire(Vector3 direction)
+    {
+        if (NumberOfPellets == 1)
+        {
+            base.OnFire(direction);
+        }
+        else
+        {
+            for (int i = 0; i < NumberOfPellets; i++)
+            {
+                var xy = Random.insideUnitCircle * ShotRadius;
+                var newDirection = direction + transform.TransformDirection(xy);
+                FireBullet(newDirection);
+            }
+            FireHaptics();
+        }
+    }
+
+    protected override void OnAmmoSocketed(HVRGrabberBase grabber, HVRGrabbable grabbable)
+    {
+        if (hasChamberedAmmo)
+        {
+            AmmoSocketedHaptics();
+        }
+        else
+        {
+            base.OnAmmoSocketed(grabber, grabbable);
+        }
+    }
+    protected override void OnAmmoSocketReleased(HVRGrabberBase grabber, HVRGrabbable grabbable)
+    {
+        if (!hasChamberedAmmo)
+        {
+            base.OnAmmoSocketReleased(grabber, grabbable);
+        }
+    }
+
     protected override void OnCockingHandleEjected()
     {
         base.OnCockingHandleEjected();
